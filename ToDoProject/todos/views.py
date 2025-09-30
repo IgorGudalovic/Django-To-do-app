37a94e07db44cce5d3e3323b39676f033acb959f
Igor <igorgudalovic@gmail.com>
2025-09-30 17:23:05 +0200

    Lisitng To Dos

--- ToDoProject/todos/views.py
+++ ToDoProject/todos/views.py
@@ -4,7 +4,8 @@ from .models import ToDo
 
 # Create your views here.
 def list_todos_items(request):
-    return render(request, 'todos/todo_list.html')
+    context = {'todo_list': ToDo.objects.all()}
+    return render(request, 'todos/todo_list.html', context)
 
 def insert_todo_item(request:HttpRequest):
     todo = ToDo(title = request.POST["content"])
